c2696cb64bc8017a620998287ef5b1e598d38e62
mhusainprdxn <husainm@prdxn.com>
2020-01-24 10:31:12 +0530

    comment added and extra code removed

--- assets/js/teamDetails.js
+++ assets/js/teamDetails.js
@@ -1,39 +1,20 @@
-var team_info= window.location.hash.substring(1);
-// fetch('https://worldcup.sfg.io/matches')
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     appendData(data);
-//   })
-//   .catch(function (err) {
-//     console.log('error: ' + err);
-//   }); 
+var team_info= window.location.hash.substring(1); // getting the team name from url
 function loadJSON() {
   var data_file = "https://worldcup.sfg.io/matches";
   var http_request = new XMLHttpRequest();
-  try{
+  try {
      // Opera 8.0+, Firefox, Chrome, Safari
-     http_request = new XMLHttpRequest();
-  }catch (e) {
-     // Internet Explorer Browsers
-     try{
-        http_request = new ActiveXObject("Msxml2.XMLHTTP");
-     }catch (e) {
-        try{
-           http_request = new ActiveXObject("Microsoft.XMLHTTP");
-        }catch (e) {
-           // Something went wrong
-           alert("Your browser broke!");
-           return false;
-        }
+    http_request = new XMLHttpRequest();
+  } catch (e) {
+      // Something went wrong
+      alert("Your browser broke!");
+      return false;        
      }
-  }
   http_request.onreadystatechange = function() {
      if (http_request.readyState == 4  ) {
-        // Javascript function JSON.parse to parse JSON data
-        var data = JSON.parse(http_request.responseText);
-        appendData(data)
+      // Javascript function JSON.parse to parse JSON data
+      var data = JSON.parse(http_request.responseText);
+      appendData(data)
      }
   }
   http_request.open("GET", data_file, true);
@@ -56,7 +37,7 @@ function appendData(data) {
       for (var i = 0; i < data[j].home_team_statistics.starting_eleven.length; i++) {
           for (var key in data[j].home_team_statistics.starting_eleven[i]) {
               if (col.indexOf(key) === -1) {
-                col.push(key);
+                col.push(key); // getting the table head
               }
           }
       }
